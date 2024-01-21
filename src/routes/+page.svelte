67f1878a778f9362798afd78ebcddb3ff623a160
Anton Klock <klock.anton@gmail.com>
2024-01-21 16:34:57 +0100

    adding skeletonUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+</script>
+
+<div class="flex flex-col justify-center items-center h-screen">
+	<p>Tauri with SvelteKit, Tailwind and Skeleton UI</p>
+</div>
